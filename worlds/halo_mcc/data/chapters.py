0362450e18092ca3260f972ffeaa5d424ac5663f
spencer2585 <140266258+spencer2585@users.noreply.github.com>
2026-06-18 09:42:30 -0400

    Update chapters.py with Halo mission

--- worlds/halo_mcc/data/chapters.py
+++ worlds/halo_mcc/data/chapters.py
@@ -12,4 +12,6 @@ class LevelData:
 LEVEL_DATA: Dict[str, LevelData] = {
     "Reveillie": LevelData(game = "CE", level = "The Pillar of Autumn", offset = constants.PILLER_OF_AUTUMN_OFFSET + 11), #101011
     "AI Constructs and Cyborgs First!": LevelData(game = "CE", level = "The Pillar of Autumn", offset = constants.PILLER_OF_AUTUMN_OFFSET + 12), #101012
+"Flawless Cowboy": LevelData(game = "CE", level = "Halo (CE)", offset = constants.HALO_CE_MISSION_OFFSET + 11), #102011
+"Reunion Tour": LevelData(game = "CE", level = "Halo (CE)", offset = constants.HALO_CE_MISSION_OFFSET + 12), #102012
 }
