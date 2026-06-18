ca4e81312965d7851059915422da1b419ae057eb
spencer2585 <spencer2585@gmail.com>
2026-06-18 06:06:02 -0400

    fix extra useless import

--- worlds/halo_mcc/data/levels.py
+++ worlds/halo_mcc/data/levels.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass, field
 from typing import List, Dict
 from . import constants
-from .constants import GUILTY_SPARK_OFFSET
-
 
 @dataclass
 class LevelData:
@@ -16,7 +14,7 @@ LEVEL_DATA: Dict[str, LevelData] = {
     "The Truth and Reconciliation": LevelData(game = "CE", offset = constants.TRUTH_AND_RECONCILIATION_OFFSET),
     "The Silent Cartographer": LevelData(game = "CE", offset = constants.SILENT_CARTOGRAPHER_OFFSET),
     "Assault on the Control Room": LevelData(game = "CE", offset = constants.ASSAULT_CONTROL_ROOM_OFFSET),
-    "343 Guilty Spark": LevelData(game = "CE", offset=GUILTY_SPARK_OFFSET),
+    "343 Guilty Spark": LevelData(game = "CE", offset= constants.GUILTY_SPARK_OFFSET),
     "The Library": LevelData(game = "CE", offset = constants.LIBRARY_OFFSET),
     "Two Betrayals": LevelData(game = "CE", offset = constants.TWO_BETRAYALS_OFFSET),
     "Keyes": LevelData(game = "CE", offset = constants.KEYS_OFFSET),
